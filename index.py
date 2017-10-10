72d6a56b76e1eb85b55264b1ed725decd2bf6817
thusnapier <31956140+thusnapier@users.noreply.github.com>
2017-10-10 13:31:37 +0100

    Update index.py

--- index.py
+++ index.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def page_not_found(error):
   return "Page not found!", 404
+
+@app.route("/")
+def title():
+  return "Private accommodation for students in Edinburgh."
+
+@app.route('/arran_house/')
+def arran_house():
+  start = '<img src="'
+  url = url_for('static', filename='arran_house.jpg')
+  end = '">'
+  return start+url+end, 200
   
 if __name__ == "__main__"
   app.run(host='0.0.0.0', debug=True)
