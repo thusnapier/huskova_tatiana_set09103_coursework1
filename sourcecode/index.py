8907c3d855ac1419cb6860ba1c6cfa1fcd2d1952
thusnapier <40207956@live.napier.ac.uk>
2017-10-24 13:38:21 +0000

    index file

--- sourcecode/index.py
+++ sourcecode/index.py
@@ -147,114 +147,26 @@ def homepage():
         }]
   return render_template('template.html', info=data), 200
 
+#To avoid hardcoding routes for every single house, we use a general path.
+#This path generates data from JSON according to the name of house.
 @app.route("/<name>/")
 def location(name):
   data = {}
   print(name)
   with open('flatsdata.json') as infile:
+  #Load data from JSON
     data = json.load(infile)
     infile.close()
     details = None
+    #Format name of hall so that URL looks nice
     for o in data:
       formattedName = o['name']
       formattedName = formattedName.replace(' ', '_')
       formattedName = formattedName.lower()
+      #Read data according to house selected
       if formattedName == name:
         details = o
   return render_template('details.html', details = details), 200
-
-#Following routes lead to the picture of every single house separately
-#localhost:5000/nameofroute
-#@app.route("/arran_house/")
-#def arran_house():
-#  start = '<img src="'
-#  url = url_for('static', filename='arran_house.jpg')
-#  end = '">'
-#  return start+url+end, 200
-
-#@app.route("/canal_point/")
-#def canal_point():
-#  start ='<img src="'
-#  url = url_for('static', filename='canal_point.jpg')
-#  end = '">'
-#  return start+url+end, 200
-
-#@app.route("/gateaway/")
-#def gateaway():
-#  start = '<img src="'
-#  url = url_for('static', filename='gateaway_apartments.jpg')
-#  end = '">'
-#  return start+url+end, 200
-
-@app.route("/haddington/")
-def haddington():
-  start = '<img src="'
-  url = url_for('static', filename='haddington_place.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/iqfountain/")
-def iqfountain():
-  start = '<img src="'
-  url = url_for('static', filename='iqfountainbridge.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/iqgrove/")
-def iqgrove():
-  start = '<img src="'
-  url = url_for('static', filename='iqgrove.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/mcdonald/")
-def mcdonald():
-  start = '<img src="'
-  url = url_for('static', filename='mcdonald.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/millhouse/")
-def millhouse():
-  start = '<img src="'
-  url = url_for('static', filename='mill_house.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/newpark/")
-def newpark():
-  start = '<img src="'
-  url = url_for('static', filename='new_park.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/nido/")
-def nido():
-  start = '<img src="'
-  url = url_for('static', filename='nido.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/potterrow/")
-def potterrow():
-  start = '<img src="'
-  url = url_for('static', filename='potterrow.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/elliott/")
-def elliott():
-  start = '<img src="'
-  url = url_for('static', filename='pure_elliott.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/studentcastle/")
-def studentcastle():
-  start = '<img src="'
-  url = url_for('static', filename='student_castle.jpg')
-  end = '">'
-  return start+url+end, 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
